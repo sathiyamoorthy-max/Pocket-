bc01f6839507ed19741337cab7fd1cb3a5cb91c3
sathiyamoorthy-max <sathiyamoorthy8020@gmail.com>
2026-07-30 22:17:11 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,6 +3,5 @@
 # 1. பைத்தான் பேக்கேஜ்களை இன்ஸ்டால் செய்தல்
 pip install -r requirements.txt
 
-# 2. Playwright பிரவுசரையும், சிஸ்டம் ஃபைல்களையும் இன்ஸ்டால் செய்தல்
+# 2. Playwright பிரவுசரை மட்டும் இன்ஸ்டால் செய்தல் (dependencies வேண்டாம்)
 python -m playwright install chromium
-python -m playwright install-deps
