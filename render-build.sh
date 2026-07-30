a008d18a4caabea8f45650555d6afd8095e30fdd
sathiyamoorthy-max <sathiyamoorthy8020@gmail.com>
2026-07-30 21:53:03 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,13 @@
-apt-get update
-apt-get install -y ffmpeg
-# Playwright-க்கான Chromium-ஐ சர்வரில் இன்ஸ்டால் செய்யும் கட்டளை
-npx playwright install chromium
+#!/usr/bin/env bash
+
+# 1. Install Python packages
+pip install -r requirements.txt
+
+# 2. Install Playwright browser
+playwright install chromium
+
+# 3. Download and Install FFmpeg for Render Linux Server
+mkdir -p bin
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xvf ffmpeg-release-amd64-static.tar.xz
+mv ffmpeg-*-amd64-static/ffmpeg bin/
