7f48302ec61ce5e81a4dffc3c02ca299e1f6b8d9
sathiyamoorthy-max <sathiyamoorthy8020@gmail.com>
2026-07-30 21:28:52 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,4 @@
-apt-get update && apt-get install -y ffmpeg
+apt-get update
+apt-get install -y ffmpeg
+# Playwright-க்கான Chromium-ஐ சர்வரில் இன்ஸ்டால் செய்யும் கட்டளை
+npx playwright install chromium
