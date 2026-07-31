4b4be045e7e0fa5dcd04d4f2a5b92e8a3f889227
sathiyamoorthy-max <sathiyamoorthy8020@gmail.com>
2026-07-31 09:46:13 +0530

    Install FFmpeg using apt-get
    Replaced manual FFmpeg installation with apt-get command.

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-
-# Render சர்வரில் FFmpeg-ஐ இன்ஸ்டால் செய்தல்
-mkdir -p bin
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-tar -xvf ffmpeg-release-amd64-static.tar.xz
-mv ffmpeg-*-amd64-static/ffmpeg bin/
+apt-get update
+apt-get install -y ffmpeg
